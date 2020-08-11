9ea2017c45328d03d886006ceb7a196788526cf8
”kazuki <Kazuki.ishigaki6228@gmail.com>
2020-08-11 09:21:48 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_form_forgery with: :exception
+  protect_from_forgery with: :exception
 
   add_flash_types :succes, :info, :warning, :danger
+  
+  helper_method :current_user, :logged_in?
+  
 end
+
+ def current_user
+   @current_user ||= User.find_by(id:session[:user_id])
+ end
+
+ def logged_in?
+  !current_user.nil?
+ end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  
   validates :name, presence: true
   validates :name, length: {maximum: 15}
   
-  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :email, presence: true
   validates :email, format: { with: VALID_EMAIL_REGEX }
   
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
   validates :password, length: {in: 8..32}
   validates :password, presence: true
   validates :password, format: { with: VALID_PASSWORD_REGEX }
